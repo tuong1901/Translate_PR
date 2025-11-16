f29f60bd84afe087440dd88a9cef3643f1468817
tuong1901 <hoanghoangcattuong@gmail.com>
2025-11-16 13:39:04 +0700

    add again file

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,12 +1,4 @@
 
-from capture import capture
-from capture.capture import capture_region
-def main():
-    print("Chương trình chính đang chạy...")
-    # Viết code chính ở đây
-    # ...
+from .capture.capture import ScreenCapture
 
-if __name__ == "__main__":
-    main() 
-    a= capture_region(100, 100, 300, 200)
-        
+ 
--- src/core/capture/__init__.py
+++ src/core/capture/__init__.py
@@ -1,8 +1,2 @@
 from .capture import ScreenCapture
-
-if __name__ == "__main__": 
-    screen_capture = ScreenCapture()
-     
-    image = screen_capture.capture_region(100, 100, 300, 200)
-     
-    image.save("captured_region.png")
+__all__ = "ScreenCapture"
